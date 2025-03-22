8ccbfb1904820452365f26e4e176f2a3aff2c197
Jesse.Feng <fxm0016@126.com>
2025-03-22 18:34:41 +0800

    feat(增强日志级别相关处理): 处理level有可能是0的情况

--- src/normalizeLevel.ts
+++ src/normalizeLevel.ts
@@ -6,12 +6,29 @@ import { TLevel } from './typings';
    * @param level Log level (日志级别)
    * @returns Normalized log level (标准化后的日志级别)
    */
-export default function normalizeLevel(level: Level | TLevel): Level {
+export default function normalizeLevel(level: Level | TLevel): Level | undefined {
+  // 可能是字符串 TRACE/DEBUG/INFO/WARN/ERROR/FATAL/OFF
+  // It might be a string TRACE/DEBUG/INFO/WARN/ERROR/FATAL/OFF
   if (typeof level === 'string') {
     level = Level[level.toUpperCase()];
-    if (level == null) {
-      level = Level.INFO;
+
+    // 不是有效的日志级别字符串
+    // It is not a valid log level string
+    if (level === void 0) {
+      return;
     }
+    return level as Level;
   }
-  return level as Level;
+
+  // 可能是数字
+  // It might be a number
+  if (typeof level === 'number') {
+    // 不是有效的日志级别枚举值
+    // It is not a valid log level enum value
+    if (Level[level] === void 0) {
+      return;
+    }
+
+    return level;
+  }
 }
