0b9a6e5d4bf6f9a3792347eac489ddd21d440c5d
Jesse.Feng <fxm0016@126.com>
2025-03-25 10:12:16 +0800

    feat(2.0.5): basicLogPrefix优化

--- src/basicLogPrefix.ts
+++ src/basicLogPrefix.ts
@@ -4,6 +4,6 @@ import { ILogEvent } from './typings';
 
 const DEFAULT_FORMATTER = 'YYYY-MM-DD HH:mm:ss.SSSZ';
 
-export default function basicLogPrefix(event: ILogEvent, dateFormat: string): string {
+export default function basicLogPrefix(event: ILogEvent, dateFormat?: string): string {
   return `[${format(event.timestamp, dateFormat || DEFAULT_FORMATTER)}] [${event.levelName}] [${event.loggerName}] -`;
 }
